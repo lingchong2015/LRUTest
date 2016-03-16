7a567cf6ef9cc2ece2b3e5b332d5f5b796f7dff2
Administrator <lingdirk2009@163.com>
2016-03-16 12:02:35 +0800

    Add HashMap implemetation.

--- src/com/curry/stephen/MyHashMapLRUCache.java
+++ src/com/curry/stephen/MyHashMapLRUCache.java
@@ -1,5 +1,7 @@
 package com.curry.stephen;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -9,5 +11,12 @@ public class MyHashMapLRUCache<K, V> {
     private final int MAX_CACHE_SIZE;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private Map<K, V> mMap;
-    private
+    private Map.Entry<K, V> mHeadEntry;
+    private Map.Entry<K, V> mTailEntry;
+
+    public MyHashMapLRUCache(int maxCacheSize) {
+        MAX_CACHE_SIZE = maxCacheSize;
+        int capability = (int) (MAX_CACHE_SIZE / DEFAULT_LOAD_FACTOR + 1);
+        mMap = Collections.synchronizedMap(new HashMap<>(capability, DEFAULT_LOAD_FACTOR));
+    }
 }

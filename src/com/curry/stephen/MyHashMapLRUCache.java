3a81700975217e48fcb7819fa8442212cfc92f13
Administrator <lingdirk2009@163.com>
2016-03-16 11:58:51 +0800

    Add HashMap implemetation.

---
+++ src/com/curry/stephen/MyHashMapLRUCache.java
@@ -0,0 +1,13 @@
+package com.curry.stephen;
+
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2016/3/16 0016.
+ */
+public class MyHashMapLRUCache<K, V> {
+    private final int MAX_CACHE_SIZE;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private Map<K, V> mMap;
+    private
+}

3e91226c27d4e388d48eba56731e1cd63e41f9f4
Administrator <lingdirk2009@163.com>
2016-03-16 11:55:21 +0800

    Change class name.

--- src/com/curry/stephen/Main.java
+++ src/com/curry/stephen/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        MyLRUCache<Integer, String> myLRUCache = new MyLRUCache<Integer, String>(3);
+        MyLinkedHashMapLRUCache<Integer, String> myLRUCache = new MyLinkedHashMapLRUCache<Integer, String>(3);
         myLRUCache.put(1, "1");
         myLRUCache.put(2, "2");
         myLRUCache.put(3, "3");
